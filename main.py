fd277f06ce660eef1096c11f3ad7a6b109efe0d3
MeltedButetr77 <MeltedButterGaming@gmail.com>
2024-01-27 00:37:15 +1030

    creating button infrastructure

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((400, 500))
 clock = pygame.time.Clock()
 
 buttons = []
 
 
-class Button():
-    def __init__(self, position, size):
+class Button:
+    def __init__(self, position, size, button_id):
         super().__init__()
+        self.id = button_id
         self.image = pygame.Surface(size)
         self.image.fill('red')
         self.rect = self.image.get_rect()
@@ -21,11 +22,40 @@ class Button():
     def highlight(self, mouse_pos):
         pass
 
-    def click(self, mouse_click):
-        pass
+    def draw(self, surface):
+        surface.blit(self.image, self.rect.topleft)
+
+    def click(self, click_pos):
+        action = False
+        # checks current mouse position collides with the button
+        if self.rect.collidepoint(click_pos):
+            # Checks left mouse button
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        return action
 
 
-Button((400, 400), (20, 20))
+Button((300, 0), (100, 100), "/")
+Button((300, 100), (100, 100), "x")
+Button((300, 200), (100, 100), "-")
+Button((300, 300), (100, 100), "+")
+Button((300, 400), (100, 100), "=")
+
+Button((0, 100), (100, 100), 1)
+Button((100, 100), (100, 100), 2)
+Button((200, 100), (100, 100), 3)
+Button((0, 200), (100, 100), 4)
+Button((100, 200), (100, 100), 5)
+Button((200, 200), (100, 100), 6)
+Button((0, 300), (100, 100), 7)
+Button((100, 300), (100, 100), 8)
+Button((200, 300), (100, 100), 9)
+Button((0, 400), (100, 100), 0)
+
+pressed = []
+expression = ""
+current_number = ""
 
 running = True
 while running:
@@ -35,8 +65,13 @@ while running:
         if event.type == pygame.MOUSEMOTION:
             for button in buttons:
                 button.highlight(event.pos)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for button in buttons:
+                if button.click(event.pos):
+                    pressed.append(button.id)
 
-    buttons.draw(screen)
+    for button in buttons:
+        button.draw(screen)
 
     pygame.display.update()
     screen.fill('black')
